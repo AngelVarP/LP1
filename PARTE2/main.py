3f35df82a92ff1fcee11b7480f90e40ec9367bd9
LAngel_VP <luis.vargas.p@uni.pe>
2025-05-06 03:01:39 -0500

    validacionFacial6

--- PARTE2/main.py
+++ PARTE2/main.py
@@ -1,6 +1,7 @@
 from validacionUsuario import ingresoUsuario
 from menu import menu
+from validacionUsuario import agregarUsuario # quitar, solo es para la prueba
 
-
+agregarUsuario()
 ingresoUsuario()
 menu()
